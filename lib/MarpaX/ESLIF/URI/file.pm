fce475e6c29e89fefd6eea8b2fe860d737406e0c
Jean-Damien Durand <Jean-Damien.Durand@newaccess.ch>
2017-07-17 15:46:33 +0200

    URI::file

--- lib/MarpaX/ESLIF/URI/file.pm
+++ lib/MarpaX/ESLIF/URI/file.pm
@@ -42,16 +42,33 @@ __DATA__
 
 <auth path>      ::= <file auth> <path absolute>
                    |             <path absolute>
+                   | <file auth> <file absolute>
+                   |             <file absolute>
+                   | <unc authority> <path absolute>
 
 <local path>     ::= <drive letter> <path absolute>
-                   | <path absolute>
+                   |                <path absolute>
+                   |                <file absolute>
+
+<unc authority>  ::= "//" <file host>                             action => authority
+                   | "///" <file host>                            action => authority
+
+<file host>      ::= <inline IP>                                  action => host
+                   | IPv4address                                  action => host
+                   | <reg name>                                   action => host
+
+<inline IP>      ::= "%5B" <IPv6address> "%5D"
+                   | "%5B" <IPvFuture> "%5D"
+
+<file absolute>  ::= "/" <drive letter> <path absolute>
 
 <drive letter>   ::= ALPHA ":"                                     action => drive
+                   | ALPHA "|"                                     action => drive
 
 <file auth>      ::= userinfo "@" <host>                           action => authority
-                   | <host>                                        action => authority
+                   |              <host>                           action => authority
 
 <host>           ::= "localhost"                                   action => host
-  #
+#
 # Generic syntax will be appended here
 #
